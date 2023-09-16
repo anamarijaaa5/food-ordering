592d75bb3e4383c5c46a57ae0e932d5a86380062
anamarijaaa5 <119890476+anamarijaaa5@users.noreply.github.com>
2023-09-16 17:51:25 +0200

    Refactor login.php

--- login.php
+++ login.php
@@ -1,82 +1,89 @@
 <!DOCTYPE html>
 <html>
 <head>
-  <title>Login</title>
-  <style>
-    body {
-      font-family: Arial, sans-serif;
-      background-color: #f5f5f5;
-    }
-
-    .container {
-      max-width: 400px;
-      margin: 0 auto;
-      padding: 20px;
-      background-color: #ffffff;
-      border-radius: 5px;
-      box-shadow: 0 2px 5px rgba(0, 0, 0, 0.1);
-      text-align: center;
-      margin-top: 100px;
-    }
-
-    h1 {
-      color: #ff0000;
-      font-size: 24px;
-    }
-
-    .button {
-      display: inline-block;
-      background-color: #333333;
-      color: #ffffff;
-      padding: 10px 20px;
-      border-radius: 5px;
-      text-decoration: none;
-      margin-top: 20px;
-    }
-  </style>
+    <title>Login</title>
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+            background-color: #f5f5f5;
+        }
+        .container {
+            max-width: 400px;
+            margin: 0 auto;
+            padding: 20px;
+            background-color: #ffffff;
+            border-radius: 5px;
+            box-shadow: 0 2px 5px rgba(0, 0, 0, 0.1);
+            text-align: center;
+            margin-top: 100px;
+        }
+        h1 {
+            color: #ff0000;
+            font-size: 24px;
+        }
+        .button {
+            display: inline-block;
+            background-color: #333333;
+            color: #ffffff;
+            padding: 10px 20px;
+            border-radius: 5px;
+            text-decoration: none;
+            margin-top: 20px;
+        }
+    </style>
 </head>
 <body>
-  <div class="container">
+<div class="container">
     <?php
-    $imeServera = "localhost";
-    $username = "rwa052023";
-    $lozinka = "csdigital2023";
-    $imeBaze = "rwa052023";
-    $KorisnickoIme=$_GET["email1"];
-    $Lozinka=$_GET["lozinka1"];
+    $dbConfig = [
+        'host' => 'localhost',
+        'user' => 'rwa052023',
+        'password' => 'csdigital2023',
+        'database' => 'rwa052023',
+    ];
 
-    // Uspostava konekcije
-    $konekcija = mysqli_connect($imeServera, $username, $lozinka, $imeBaze);
-    // Provjera konekcije
-    if (!$konekcija) {
-      die("Konekcija neuspjela: " . mysqli_connect_error());
+    $conn = new mysqli($dbConfig['host'], $dbConfig['user'], $dbConfig['password'], $dbConfig['database']);
+    
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
     }
 
-    $result = $konekcija->query("SELECT * FROM Korisnik WHERE KorisnickoIme = '$KorisnickoIme' AND Lozinka = '$Lozinka'");
+    $KorisnickoIme = $_GET['email1'] ?? null;
+    $Lozinka = $_GET['lozinka1'] ?? null;
 
-    if (mysqli_num_rows($result) == 1) {
-        session_start();
-        $_SESSION["login"] = true;
-        $_SESSION["KorisnickoIme"] = $KorisnickoIme;
-        $row = mysqli_fetch_assoc($result);	
-        $_SESSION["KorisnikID"] = $row["ID"];
+    if ($KorisnickoIme && $Lozinka) {
+        $stmt = $conn->prepare('SELECT * FROM Korisnik WHERE KorisnickoIme = ? AND Lozinka = ?');
+        $stmt->bind_param('ss', $KorisnickoIme, $Lozinka);
+        $stmt->execute();
+        $result = $stmt->get_result();
 
-        $musterija = $konekcija->query("SELECT * FROM Korisnik WHERE KorisnickoIme = '$KorisnickoIme' AND Lozinka = '$Lozinka' AND Uloga='Musterija'");
-        if (mysqli_num_rows($musterija) == 1) {
-            $_SESSION["uloga"] = 'Musterija';
-            echo "<script> window.location.assign('index.php'); </script>";
+        if ($result->num_rows === 1) {
+            session_start();
+            $_SESSION['login'] = true;
+            $_SESSION['KorisnickoIme'] = $KorisnickoIme;
+            $row = $result->fetch_assoc();
+            $_SESSION['KorisnikID'] = $row['ID'];
+
+            $stmt->close();
+            $stmt = $conn->prepare('SELECT * FROM Korisnik WHERE KorisnickoIme = ? AND Lozinka = ? AND Uloga = ?');
+            $role = 'Musterija';
+            $stmt->bind_param('sss', $KorisnickoIme, $Lozinka, $role);
+            $stmt->execute();
+            $result = $stmt->get_result();
+
+            $_SESSION['uloga'] = $result->num_rows === 1 ? 'Musterija' : 'Administrator';
+            header('Location: index.php');
         } else {
-            $_SESSION["uloga"] = 'Administrator';
-            echo "<script> window.location.assign('index.php'); </script>";
+            echo "<h1>Neuspješna prijava!</h1>";
+            echo "<p>Vratite se nazad i pokušajte ponovno.</p>";
+            echo "<a href='index.php' class='button'>Go Back to Login</a>";
         }
-    } else {
-        echo "<h1>Neuspješna prijava!</h1>";
-        echo "<p> Vratite se nazad i pokušajte ponovno.</p>";
-        echo "<a href='index.php' class='button'>Go Back to Login</a>";
+        
+        $stmt->close();
     }
 
-    $konekcija->close();
+    $conn->close();
     ?>
-  </div>
+</div>
 </body>
 </html>
