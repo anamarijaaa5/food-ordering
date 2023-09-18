b2aeccf981960c0da7b5bdd3f94789a2ae8307b7
anamarijaaa5 <119890476+anamarijaaa5@users.noreply.github.com>
2023-09-18 21:13:38 +0200

    Update updateuloga.php

--- updateuloga.php
+++ updateuloga.php
@@ -1,10 +1,12 @@
 <html> 
 <body>
 <?php
+error_reporting(E_ALL);
+ini_set('display_errors', '1');
+ini_set('display_startup_errors', '1');
+
  ob_start();
  session_start(); 
-
-
 
 	 if($_SESSION["uloga"] != 'Administrator')
 	{
